de4c5fd9c8e216d5e10d25257186f93fc4cd7af0
ryan <4721319+rpshukla@users.noreply.github.com>
2019-03-28 15:44:00 -0600

    Add comments to processbuffer

--- src/processbuffer.h
+++ src/processbuffer.h
@@ -1,9 +1,24 @@
+// -------------------------------------------------
+// Name: Mitchell Adam, Ryan Shukla
+// ID: 1528592, 1537980
+// CMPUT 275, Winter 2018
+//
+// Final Project
+// -------------------------------------------------
+
 #ifndef _PROCESS_BUFFER_H_
 #define _PROCESS_BUFFER_H_
 
 #include "fourier.h"
 #include "fundamental.h"
 
+/**
+ * Does all signal processing on buffer.
+ * Note: this function modifies buffer
+ * @param buffer Pointer to the start of the buffer
+ * @param bufferLen Length of the buffer
+ * @param channels Number of audio channels in buffer
+ */
 void processBuffer(double *buffer, std::size_t bufferLen, int channels);
 
 #endif
